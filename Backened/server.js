08e6699f9a16cdaec88dce42ba22b87da30af589
Himanshu <himanshusingh2001ad@gmail.com>
2024-08-11 20:49:21 +0530

    cors security added

--- Backened/server.js
+++ Backened/server.js
@@ -24,6 +24,7 @@ app.use((req,res,next)=>{
      res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+     next();
 })
 
 mongoose.connect(db_link).then(()=>{
